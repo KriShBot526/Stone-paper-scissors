59c8b9970cfbe82bb351e9fcf461f1428d33ac18
Krishna Singh <78954802+KriShBot526@users.noreply.github.com>
2022-10-25 01:59:59 +0530

    Update Stone paper scissor.py

--- Stone paper scissor.py
+++ Stone paper scissor.py
@@ -1,32 +1,48 @@
 import random
+import regex as re
 
 user_score = 0
 comp_score = 0
 
 while True:
-    rand = random.choice(["rock", "paper", "scissor"])
+    print("\n")
+    print("Rock, Paper, Scissor... Shoot!")
 
-    user_input = input("Enter Rock/Paper/Scissor or press q to Quit: ").lower()
-    if user_input == "q":
+    user_input = input("Enter [R]ock/[P]aper/[S]cissor or press to [Q]uit: ")
+    
+    if(not re.match("[RrPpSsQq]", user_input)) or (len(user_input) != 1):
+        print("Please Choose a letter")
+        print("[R]ock/[P]aper/[S]cissor or [Q]uit")
+        continue
+
+    print(f"You chose {user_input}")
+
+    if user_input.upper() == "Q":
+        print("Exiting Game...")
         print(f"Your score is {user_score} and computer score is {comp_score}")
         quit()
 
-    if user_input in ["rock", "paper", "scissor"]:
-        if rand == user_input:
-            print("Ended in a draw")
+    choices = ["R", "P", "S"]
 
-        elif rand == "rock" and user_input == "paper":
-            print("You win!!!")
-            user_score += 1
-        elif rand == "paper" and user_input == "scissor":
-            print("You win!!!")
-            user_score += 1
-        elif rand == "scissor" and user_input == "rock":
-            print("You win!!!")
-            user_score += 1
+    comp_choice = random.choice(choices)
+    print(f"I chose {comp_choice}")
+
+
+    if comp_choice == user_input.upper():
+        print("Ended in a draw")
+
+    elif comp_choice == "R" and user_input.upper() == "S":
+        print("Rock beats scissors! I win!")
+        comp_score += 1
+
+    elif comp_choice == "S" and user_input.upper() == "P":
+        print("Scissors beats paper! I win!")
+        comp_score += 1
+
+    elif comp_choice == "R" and user_input.upper() == "S":
+        print("Paper beat rock, I win!")
+        comp_score += 1
         
-        else:
-            print("Computer wins!!!")
-            comp_score += 1
     else:
-        print("Wrong input!!!")
+        print("You win!")
+        user_score += 1
